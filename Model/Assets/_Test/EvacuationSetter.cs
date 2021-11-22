48c35bcb8cb2c3c491e1982b625ff6d85c9c69bb
Zolbayar Zamilan <rekzamilan@gmail.com>
2021-11-23 03:42:31 +0900

    Add empty project

---
+++ Model/Assets/_Test/EvacuationSetter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using Mapbox.Unity.MeshGeneration.Interfaces;
+
+public class EvacuationSetter : MonoBehaviour, IFeaturePropertySettable
+{
+    public Text[] setValueTexts;
+    public Text[] activateTexts;
+
+    private Dictionary<GameObject, string> objectKeyMap = new Dictionary<GameObject, string>();
+
+    void Awake()
+    {
+        foreach(var text in setValueTexts.Concat(activateTexts))
+        {
+            objectKeyMap[text.gameObject] = text.text;
+        }
+    }
+
+    public void Set(Dictionary<string, object> props)
+    {
+        foreach(var text in setValueTexts)
+        {
+            string key = objectKeyMap[text.gameObject];
+            if (props.ContainsKey(key))
+            {
+                text.text = props[key].ToString();
+            }
+        }
+        foreach (var text in activateTexts)
+        {
+            string key = objectKeyMap[text.gameObject];
+            bool active = props.ContainsKey(key) && props[key].ToString() == "◎";
+            text.gameObject.SetActive(active);
+        }
+    }
+}
---
+++ Model/Assets/_Test/ParkInfoSetter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mapbox.Unity.MeshGeneration.Interfaces;
+
+public class ParkInfoSetter : MonoBehaviour, IFeaturePropertySettable
+{
+    public Text title;
+    public Text description;
+
+    public void Set(Dictionary<string, object> props)
+    {
+        if(props.ContainsKey("title"))
+        {
+            title.text = props["title"].ToString();
+        }
+        if(props.ContainsKey("description"))
+        {
+            description.text = props["description"].ToString();
+        }
+    }
+}
