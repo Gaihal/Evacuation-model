c06ffc70874b1578948c8521a77889ff58bfbbac
Zolbayar Zamilan <rekzamilan@gmail.com>
2021-12-07 14:24:49 +0900

    Navigation

---
+++ Model/Assets/_NavDemo/NavTEset.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mapbox.Utils;
+using Mapbox.Map;
+using Mapbox.Unity.Utilities;
+using Mapbox.Unity.Map;
+
+public class NavTEset : MonoBehaviour
+{
+	[SerializeField]
+	AbstractMap _map;
+
+	[SerializeField]
+	[Geocode]
+	string[] _locationStrings;
+	Vector2d[] _locations;
+
+	void Start()
+	{
+		_locations = new Vector2d[_locationStrings.Length];
+
+		for (int i = 0; i < _locationStrings.Length; i++)
+		{
+			var locationString = _locationStrings[i];
+			_locations[i] = Conversions.StringToLatLon(locationString);
+			transform.localPosition = _map.GeoToWorldPosition(_locations[i], true);
+
+		}
+		//var map = Instance.mapManager;
+		//transform.localPosition = map.GeoToWorldPosition(LocationProvider.CurrentLocation.LatitudeLongitude);
+
+
+
+	}
+
+}
